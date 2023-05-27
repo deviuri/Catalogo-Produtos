a65971b387400e9645d3f39a1e737b00bed6506a
Iuri Martins <114511281+deviuri@users.noreply.github.com>
2023-05-27 02:22:46 -0300

    .

--- src/test/java/com/catalogo/servicos/ProdutoServicoTests.java
+++ src/test/java/com/catalogo/servicos/ProdutoServicoTests.java
@@ -1,5 +1,7 @@
 package com.catalogo.servicos;
 
+import com.catalogo.Infra.exceptions.DatabaseException;
+import com.catalogo.Infra.exceptions.ResourceNotFoundException;
 import com.catalogo.repository.ProdutosRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 public class ProdutoServicoTests {
@@ -20,23 +24,47 @@ public class ProdutoServicoTests {
     private ProdutosRepository repository;
 
     private long IdExiste;
-    private long Naoexiste;
+    private long IdNaoexiste;
+    private long dependeId;
     @BeforeEach
     void setUp() throws Exception{
         IdExiste = 1L;
-        Naoexiste = 1000L;
+        IdNaoexiste = 1000L;
+        dependeId = 4L;
 
-        Mockito.doNothing().when(repository).deleteById(IdExiste);
+        doNothing().when(repository).deleteById(IdExiste);
+        
+        doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(IdNaoexiste);
 
-        Mockito.doNothing().doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(Naoexiste);
+        doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependeId);
     }
 
     @Test
-    public void NaoDeveFazerNadaQuandoIdExistir() throws RuntimeException{
+    public void DeveLancarDatabaseExceptionQuandoNaoExistirId(){
+
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            servico.deletarProduto(dependeId);
+        });
+
+        verify(repository, times(1)).deleteById(dependeId);
+    }
+
+    @Test
+    public void DeveLancarResouceNotFoundExceptionQuandoNaoExistirId(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            servico.deletarProduto(IdNaoexiste);
+        });
+
+        verify(repository, times(1)).deleteById(IdNaoexiste);
+    }
+    @Test
+    public void NaoDeveFazerNadaQuandoIdExistir(){
+
         Assertions.assertDoesNotThrow(() -> {
             servico.deletarProduto(IdExiste);
         });
 
-        Mockito.verify(repository, Mockito.times(1)).deleteById(IdExiste);
+        verify(repository, times(1)).deleteById(IdExiste);
     }
 }
